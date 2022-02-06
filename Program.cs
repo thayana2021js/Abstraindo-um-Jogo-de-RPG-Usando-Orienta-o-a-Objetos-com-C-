f38fd9839f0fb37c70c86457793d157635ac8bc6
Thayana Vilaça Fortunato <76934028+thayana2021js@users.noreply.github.com>
2022-02-06 03:48:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//PROGRAMA ORIENTAÇÃO A OBJETO- PROJETO_DIO_
+using System;
+using DOTNET__POO.src.Entities;
+
+namespace Dotnet_POO
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+
+            //Programa orientado a objeto.Paramentros.
+
+            Knight arus = new Knight("Arus", 23, "Knigth");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+            
+
+            Console.WriteLine(arus.Attack()); 
+            Console.WriteLine(wizard.Attack(1));
+
+        }
+
+    }
+}
+
+ 
